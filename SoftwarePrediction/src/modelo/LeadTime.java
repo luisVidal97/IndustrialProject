e9bc7fb0399d0975073fdebc6419652b91f61778
juancamil426 <42019476+juancamil426@users.noreply.github.com>
2019-05-06 00:20:50 -0500

    Los dos primeros cuadros de lead time

--- SoftwarePrediction/src/modelo/LeadTime.java
+++ SoftwarePrediction/src/modelo/LeadTime.java
@@ -12,6 +12,25 @@ public class LeadTime
 	private ArrayList<Articulo> articulos;
 	private double[] listaDemoras;
 	
+	////////////////////////////////////////////
+	
+	private int numeroDemoras;
+	private double valorMaximo;
+	private double valorMinimo;
+	private double valorRango;
+	private double numeroIntervalos;
+	private double ancho;
+	
+	
+	////////////////////////////////////////
+	
+	private double[] limiteInferior;
+	private double[] limiteSuperior;
+	private double[] marcaClase;
+	private double[] numeroOcurrencias;
+	private double[] probaOcurre;
+	private double[] cuadrados;
+	
 	public LeadTime(double r, double tiempoTransito, double nacionalizaciónIngreso, double nivelServicio, double z,ArrayList<Articulo> articulos, double[] listaDemoras) {
 		super();
 		R = r;
@@ -21,8 +40,109 @@ public class LeadTime
 		Z = z;
 		this.articulos = articulos;
 		this.listaDemoras = listaDemoras;
+		
+		numeroDemoras = listaDemoras.length;
+		valorMaximo = listaDemoras[0];
+		valorMinimo = listaDemoras[0];
+		valorRango = 0;
+		numeroIntervalos = 0;
+		ancho = 0;
+		
+		llenarPrimerCuadro();
+		
+		
 	}
 	
+	private void llenarPrimerCuadro() 
+	{
+		for(int i = 0; i < listaDemoras.length; i++)
+		{
+			if(valorMinimo>listaDemoras[i])
+			{
+				valorMinimo=listaDemoras[i];
+			}
+			if(valorMaximo<listaDemoras[i])
+			{
+				valorMaximo=listaDemoras[i];
+			}
+		}
+		
+		valorRango = valorMaximo - valorMinimo;
+		numeroIntervalos = Math.ceil(1+(3.3*(Math.log(numeroDemoras) / Math.log(10))));
+		ancho = Math.ceil(valorRango/numeroIntervalos);
+		
+		System.out.println("tamaño: " + listaDemoras.length);
+		System.out.println("min: " + valorMinimo);
+		System.out.println("max: " + valorMaximo);
+		System.out.println("rango: " + valorRango);
+		System.out.println("intervalor: " + numeroIntervalos);
+		System.out.println("ancho: " + ancho);
+		
+		llenarSegundoCuadro();
+		
+	}
+
+	private void llenarSegundoCuadro() 
+	{
+		limiteInferior = new double[(int) numeroIntervalos];
+		limiteSuperior = new double[(int) numeroIntervalos];
+		marcaClase = new double[(int) numeroIntervalos];
+		numeroOcurrencias = new double[(int) numeroIntervalos];
+		probaOcurre = new double[(int) numeroIntervalos];
+		cuadrados = new double[(int) numeroIntervalos];
+		
+		for(int i = 0; i < limiteInferior.length; i++)
+		{
+			if(i==0)
+			{
+				limiteInferior[i] = valorMinimo;
+				limiteSuperior[i] = limiteInferior[i] + ancho;
+				marcaClase[i] = (limiteInferior[i]  + limiteSuperior[i] )/2;
+				numeroOcurrencias[i] = calcularOcurrencias(limiteInferior[i] , limiteSuperior[i] , 0);
+				probaOcurre[i] = numeroOcurrencias[i] / numeroDemoras;
+				cuadrados[i] = marcaClase[i] * marcaClase[i];
+			}
+			else
+			{
+				limiteInferior[i] = limiteSuperior[i-1];
+				limiteSuperior[i] = limiteInferior[i] + ancho;
+				marcaClase[i] = (limiteInferior[i]  + limiteSuperior[i] )/2;
+				numeroOcurrencias[i] = calcularOcurrencias(limiteInferior[i] , limiteSuperior[i], 1 );
+				probaOcurre[i] = numeroOcurrencias[i] / numeroDemoras;
+				cuadrados[i] = marcaClase[i] * marcaClase[i];
+			}
+			
+			System.out.println(limiteInferior[i] + "  " +  limiteSuperior[i] + "  " + marcaClase[i] + "  " + numeroOcurrencias[i] + "  " + probaOcurre[i] + "  " +  cuadrados[i]);
+		}
+		
+	}
+
+	private double calcularOcurrencias(double inferior, double superior, int indic) 
+	{
+		double ocurren = 0;
+		
+		for(int i = 0; i < listaDemoras.length; i++)
+		{
+			
+			if(indic == 0)
+			{
+				if(listaDemoras[i] >= inferior && listaDemoras[i] <= superior)
+				{
+					ocurren++;
+				}
+			}
+			else
+			{
+				if(listaDemoras[i] > inferior && listaDemoras[i] <= superior)
+				{
+					ocurren++;
+				}
+			}
+		}
+		
+		return ocurren;
+	}
+
 	public double getR() {
 		return R;
 	}
